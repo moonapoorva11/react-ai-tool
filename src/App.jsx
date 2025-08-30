f28b4932e8ecbbb6822266653939f999507fb0a2
moon123apoorva <moon2018apoorva@gmail.com>
2025-08-30 16:15:03 +0530

    make correction in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from "./Components/sidebar/Sidebar";
+import Sidebar from "./Componets/sidebar/Sidebar";
 import Main from './Componets/sidebar/main/Main'
 
 const App = () => {
